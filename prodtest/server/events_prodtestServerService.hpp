ded54299e1b002fc2676cc24ff9ad07032548039
akakist <akakist@gmail.com>
2022-12-01 12:47:20 +0300

    zzz

--- prodtest/server/events_prodtestServerService.hpp
+++ prodtest/server/events_prodtestServerService.hpp
@@ -6,11 +6,21 @@ inline std::set<EVENT_id> getEvents_prodtestServerService()
 {
 
 	std::set<EVENT_id> out;
+	out.insert(prodtestEventEnum::AddTaskREQ);
+	out.insert(prodtestEventEnum::AddTaskRSP);
+	out.insert(rpcEventEnum::IncomingOnAcceptor);
+	out.insert(systemEventEnum::startService);
+	out.insert(timerEventEnum::TickTimer);
 
 	return out;
 }
 
 inline void regEvents_prodtestServerService()
 {
+	iUtils->registerEvent(prodtestEvent::AddTaskREQ::construct);
+	iUtils->registerEvent(prodtestEvent::AddTaskRSP::construct);
+	iUtils->registerEvent(rpcEvent::IncomingOnAcceptor::construct);
+	iUtils->registerEvent(systemEvent::startService::construct);
+	iUtils->registerEvent(timerEvent::TickTimer::construct);
 }
 #endif
